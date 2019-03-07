2ff64fdda43ce25f997c6484defd8cd454bd73b0
liliang <liliang8858@126.om>
2019-03-07 23:09:40 +0800

    1.1.0 封装连接api  channel应用到 内部读协程 和写协程

---
+++ impl/connection.go
@@ -0,0 +1,113 @@
+package impl
+
+import (
+	"errors"
+	"github.com/gorilla/websocket"
+	"sync"
+)
+
+type Connection struct {
+	wsConn *websocket.Conn
+	inChan chan []byte
+	outChan chan [] byte
+	closeChan chan byte
+
+	mutex sync.Mutex
+	isClose bool
+}
+
+func InitConnetcion(wsConn *websocket.Conn) (conn *Connection, err error) {
+	conn = &Connection{
+		wsConn:wsConn,
+		inChan:make(chan []byte,1000),
+		outChan:make(chan []byte,1000),
+		closeChan:make(chan byte ,1),
+	}
+
+	// 拉起内部 读协程
+	go conn.readLoop()
+	// 拉起内部 写协程
+	go conn.writeLop()
+	return
+}
+
+func (conn *Connection)ReadMessage()(data[]byte,err error)  {
+	select {
+	case data = <- conn.inChan:
+	case <-conn.closeChan:
+		err = errors.New("connection is closed")
+
+	}
+	return
+}
+
+func (conn *Connection)WriterMessage(data[] byte)(err error)  {
+
+	select {
+	case conn.outChan <- data:
+	case <-conn.closeChan:
+		err = errors.New("connection is closed")
+
+	}
+	return
+}
+func (conn*Connection)Close()  {
+	// 线程安全，可重入的close
+	conn.wsConn.Close()
+
+	// 关闭底层长连接，还会关闭 closeChan
+	// 有可能多次关闭
+	// 保证执行一次，枷锁
+	conn.mutex.Lock()
+	if !conn.isClose {
+		close(conn.closeChan)
+		conn.isClose = true
+	}
+	conn.mutex.Unlock()
+
+
+}
+
+// 内部实现  读协程
+func (conn *Connection)readLoop()  {
+	var(
+		data []byte
+		err error
+	)
+	for{
+		if _,data,err =conn.wsConn.ReadMessage(); err!=nil{
+			goto ERR
+		}
+
+		select {
+		case conn.inChan <- data:
+		case <-conn.closeChan:
+			// 当 closechan 被关闭了
+			goto ERR
+		}
+
+
+	}
+	ERR:
+		conn.Close()
+}
+
+func (conn *Connection) writeLop() {
+	var(
+		data []byte
+		err error
+	)
+	for{
+		select {
+		case data= <-conn.outChan:
+		case <-conn.closeChan:
+			goto ERR
+		}
+		if err= conn.wsConn.WriteMessage(websocket.TextMessage,data);err != nil{
+			goto ERR
+		}
+	}
+	ERR:
+		conn.Close()
+}
+
--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"github.com/im-ai/pushm/impl"
 	"net/http"
+	"time"
 )
 
 var (
@@ -19,27 +21,48 @@ var (
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 	var (
-		conn *websocket.Conn
+		wsConn *websocket.Conn
 		err error
 		data []byte
+		conn *impl.Connection
 	)
 
 	// 升级 为 websocket 请求
-	if conn,err = upgrader.Upgrade(w,r,nil); err!=nil{
+	if wsConn,err = upgrader.Upgrade(w,r,nil); err!=nil{
 		return
 	}
 
-	for   {
-		if _,data,err =  conn.ReadMessage(); err != nil{
+	// 初始化
+	if conn, err = impl.InitConnetcion(wsConn); err != nil {
+		goto ERR
+	}
+
+	go func() {
+		var (
+			err error
+		)
+		for   {
+			if err = conn.WriterMessage([]byte("hearbeat"));err !=nil{
+				return
+			}
+			time.Sleep(1*time.Second)
+		}
+
+	}()
+
+
+
+	for{
+		if data,err = conn.ReadMessage();err !=nil{
 			goto ERR
 		}
-		if err = conn.WriteMessage(websocket.TextMessage,data);err != nil {
+		if err = conn.WriterMessage(data);err != nil{
 			goto ERR
 		}
 	}
 
-	ERR:
-		conn.Close()
+ERR:
+	conn.Close()
 }
 
 func main() {
