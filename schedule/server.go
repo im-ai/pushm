53083cc78b5ed3e4b8c1e88dac397c559afc36a0
liliang <liliang8858@126.com>
2019-03-26 09:16:10 +0800

    1.0.0 add metrics

--- schedule/server.go
+++ schedule/server.go
@@ -5,14 +5,20 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/shirou/gopsutil/mem"
 	"hash/crc32"
 	"io"
 	"io/ioutil"
+	"log"
 	"net"
+	"net/http"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var (
@@ -35,6 +41,9 @@ func main() {
 		return
 	}
 
+	// 启动 指标服务器
+	go initMetrics()
+
 	//类似于初始化套接字，绑定端口
 	tcpServer := initTcpServer()
 	//记得关闭
@@ -46,6 +55,45 @@ func main() {
 		fmt.Println("accept tcp client %s", conn.RemoteAddr().String())
 		checkErr(err)
 		go Handle(conn)
+	}
+
+}
+
+func initMetrics() {
+	//初始化日志服务
+	logger := log.New(os.Stdout, "[Memory]", log.Lshortfile|log.Ldate|log.Ltime)
+
+	//初始一个http handler
+	http.Handle("/metrics", promhttp.Handler())
+	//初始化一个容器
+	diskPercent := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "go_memeory_percent",
+		Help: "memeory use percent",
+	},
+		[]string{"percent"},
+	)
+	prometheus.MustRegister(diskPercent)
+
+	// 启动web服务，监听1010端口
+	go func() {
+		logger.Println("ListenAndServe at:192.168.1.70:1010")
+		err := http.ListenAndServe(":1010", nil)
+		if err != nil {
+			logger.Fatal("ListenAndServe: ", err)
+		}
+	}()
+
+	//收集内存使用的百分比
+	for {
+		logger.Println("start collect memory used percent!")
+		v, err := mem.VirtualMemory()
+		if err != nil {
+			logger.Println("get memeory use percent error:%s", err)
+		}
+		usedPercent := v.UsedPercent
+		logger.Println("get memeory use percent:", usedPercent)
+		diskPercent.WithLabelValues("usedMemory").Set(usedPercent)
+		time.Sleep(time.Second * 2)
 	}
 
 }
